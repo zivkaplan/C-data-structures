032565c5baa4ba19bf78f7de9d74badd6801ae78
ziv.kaplan <zivkaplan@gmail.com>
2022-02-19 17:41:20 +0200

    singly list and doubly list header updated

--- doubly_linked_list/doubly_linked_list.h
+++ doubly_linked_list/doubly_linked_list.h
@@ -3,39 +3,77 @@
 
 #include <stddef.h> /* size_t */
 
-/**************************************************************
- * version 1.0                                                *
- *************************************************************/
+/***********************************
+ * Doubly Linked List Data Structure
+ * ------------------------
+ *      Implementation in C
+ *      elements are stored By reference.
+ *
+ * Ziv Kaplan
+ * February 2022
+ **********************************/
 
 typedef struct doubly_list doubly_list_t;
 typedef struct node *dll_iter_t;
 
 doubly_list_t *DoublyListCreate(void);
+/* returns NULL on failure.*/
+
 void DoublyListDestroy(doubly_list_t *list);
+/* Calling Destroy(NULL) is valid */
 
 dll_iter_t DoublyListInsert(doubly_list_t list, dll_iter_t iter, void *data);
+/*
+On success returns iterator to the new inserted data,
+on failure returns ListEnd().
+To insert the first element use only Insert(ListEnd).
+*/
+
 dll_iter_t DoublyListRemove(doubly_list_t list, dll_iter_t iter);
+/*
+Returns next iterator.
+Removing ListEnd is undefined.
+*/
 
 size_t DoublyListSize(const doubly_list_t *list);
 int DoublyListIsEmpty(const doubly_list_t *list);
 
 void *DoublyListGetData(dll_iter_t iter);
 void *DoublyListSetData(dll_iter_t iter);
+/*
+Calling SetData/GetData on an empty list is undefined.
+Calling SetData/GetData on ListEnd list is undefined.
+*/
 
 dll_iter_t DoublyListBegin(const doubly_list_t *list);
 dll_iter_t DoublyListEnd(const doubly_list_t *list);
 dll_iter_t DoublyListNext(dll_iter_t iter);
 dll_iter_t DoublyListPrev(dll_iter_t iter);
 int DoublyListIsSameIterator(const dll_iter_t iter1, const dll_iter_t iter2);
+/*
+After any change in the list (inserting or removing data),
+any existing iterators are invalidated and using them is undefined behaviour.
+*/
 
 dll_iter_t DoublyListFind(dll_iter_t from_iter, dll_iter_t to_iter,
                           void *param,
                           int (*is_match)(const void *data, const void *param));
+
+/*
+Returns the first matched iterator, or ListEnd if case no match found.
+*/
+
 int DoublyListFindAll(const doubly_list_t *list,
                       void *param,
-                      int (*is_match)(const void *data, const void *find_data), doubly_list_t *output_list);
+                      int (*is_match)(const void *data, const void *param), doubly_list_t *output_list);
+
 int DoublyListForEach(dll_iter_t from_iter, dll_iter_t to_iter,
                       void *param,
                       int (*operation_func)(void *data, void *param));
+/*
+Iteration continues while operation_func returns 0.
+Returns ListEnd if iterated through the entire list,
+otherwise returns the iterator on which it was stopped.
+*/
 
 #endif /* __DOUBLY_LINKED_LIST_H__ */
--- singly_linked_list/singly_linked_list.h
+++ singly_linked_list/singly_linked_list.h
@@ -17,16 +17,16 @@ typedef struct singly_list singly_list_t;
 typedef struct node *sll_iter_t;
 
 singly_list_t *SinglyListCreate(void);
-/*
-returns NULL on failure.
-*/
+/* returns NULL on failure.*/
 
 void SinglyListDestroy(singly_list_t *list);
 /* Calling Destroy(NULL) is valid */
 
 sll_iter_t SinglyListInsert(singly_list_t *list, sll_iter_t iter, const void *data);
 sll_iter_t SinglyListInsertAfter(singly_list_t *list, sll_iter_t iter, const void *data);
-/* returns the new inserted data on success, or list end on failure.
+/*
+On success returns iterator to the new inserted data,
+on failure returns ListEnd().
 To insert the first element use only Insert(ListEnd).
 InsertAfter(ListBegin) on an empty list is undefined.
 InsertAfter(ListEnd) on any list is undefined.
@@ -35,13 +35,18 @@ InsertAfter(ListEnd) on any list is undefined.
 sll_iter_t SinglyListRemove(singly_list_t *list, sll_iter_t iter);
 sll_iter_t SinglyListRemoveAfter(singly_list_t *list, sll_iter_t iter);
 /*
-return value is the previous iterator.
+Remove() returns the next iterator.
+RemoveAfter() returns the previous iterator (meaning updated iterator from param).
 Removing ListEnd is undefined.
 Calling any Remove method on an empty list is undefined.
 */
 
 void *SinglyListGetData(const sll_iter_t iter);
 void SinglyListSetData(sll_iter_t iter, const void *data);
+/*
+Calling SetData/GetData on an empty list is undefined.
+Calling SetData/GetData on ListEnd list is undefined.
+*/
 
 sll_iter_t SinglyListBegin(const singly_list_t *list);
 sll_iter_t SinglyListEnd(const singly_list_t *list);
@@ -55,14 +60,14 @@ any existing iterators are invalidated and using them is undefined behaviour.
 sll_iter_t SinglyListFind(const singly_list_t *list, void *param,
                           int (*is_match_func)(const void *iterated_data, const void *param));
 /*
-returns ListEnd If No match found.
+Returns the first matched iterator, or ListEnd if case no match found.
 */
 
 sll_iter_t SinglyListForEach(singly_list_t *list, void *param,
                              int (*operation_func)(void *iterated_data, void *param));
 /*
-iteration continue while oper_func returns 0.
-returns ListEnd if iterated the entire list,
+Iteration continues while operation_func returns 0.
+Returns ListEnd if iterated through the entire list,
 otherwise returns the iterator on which it was stopped.
 */
 
