505153535c7a9545e828a6e4c461c5ce56948c08
ziv.kaplan <zivkaplan@gmail.com>
2022-03-30 10:23:26 +0300

    added comments before declaration

--- doubly_linked_list/doubly_linked_list.h
+++ doubly_linked_list/doubly_linked_list.h
@@ -16,69 +16,90 @@
 typedef struct doubly_list doubly_list_t;
 typedef struct dll_node *dll_iter_t;
 
-doubly_list_t *DoublyListCreate(void);
 /* returns NULL on failure.*/
+doubly_list_t *DoublyListCreate(void);
 
-void DoublyListDestroy(doubly_list_t *list);
 /* Calling Destroy(NULL) is valid */
+void DoublyListDestroy(doubly_list_t *list);
 
-dll_iter_t DoublyListInsert(doubly_list_t *list, dll_iter_t iter, const void *data);
 /*
 On success returns iterator to the new inserted data,
 on failure returns ListEnd().
 To insert the first element use only Insert(ListEnd).
 */
+dll_iter_t DoublyListInsert(doubly_list_t *list, dll_iter_t iter, const void *data);
 
-dll_iter_t DoublyListRemove(doubly_list_t *list, dll_iter_t iter);
 /*
 Returns next iterator.
 Removing ListEnd is undefined.
 */
+dll_iter_t DoublyListRemove(doubly_list_t *list, dll_iter_t iter);
 
 size_t DoublyListGetSize(const doubly_list_t *list);
 int DoublyListIsEmpty(const doubly_list_t *list);
 
-void *DoublyListGetData(dll_iter_t iter);
-void DoublyListSetData(dll_iter_t iter, const void *data);
 /*
-Calling SetData/GetData on an empty list is undefined.
-Calling SetData/GetData on ListEnd list is undefined.
+Calling GetData on an empty list is undefined.
+Calling GetData on ListEnd list is undefined.
 */
+void *DoublyListGetData(dll_iter_t iter);
 
-dll_iter_t DoublyListBegin(const doubly_list_t *list);
-dll_iter_t DoublyListEnd(const doubly_list_t *list);
-dll_iter_t DoublyListNext(dll_iter_t iter);
-dll_iter_t DoublyListPrev(dll_iter_t iter);
-int DoublyListIsSameIterator(const dll_iter_t iter1, const dll_iter_t iter2);
+/*
+Calling SetData on an empty list is undefined.
+Calling SetData on ListEnd list is undefined.
+*/
+void DoublyListSetData(dll_iter_t iter, const void *data);
+
+
 /*
 After any change in the list (inserting or removing data),
 any existing iterators are invalidated and using them is undefined behaviour.
 */
+dll_iter_t DoublyListBegin(const doubly_list_t *list);
 
-dll_iter_t DoublyListFind(const doubly_list_t *list, void *param,
-                          int (*is_match)(const void *data, const void *param));
+/*
+After any change in the list (inserting or removing data),
+any existing iterators are invalidated and using them is undefined behaviour.
+*/
+dll_iter_t DoublyListEnd(const doubly_list_t *list);
+
+/*
+After any change in the list (inserting or removing data),
+any existing iterators are invalidated and using them is undefined behaviour.
+*/
+dll_iter_t DoublyListNext(dll_iter_t iter);
+
+/*
+After any change in the list (inserting or removing data),
+any existing iterators are invalidated and using them is undefined behaviour.
+*/
+dll_iter_t DoublyListPrev(dll_iter_t iter);
+
+int DoublyListIsSameIterator(const dll_iter_t iter1, const dll_iter_t iter2);
 
 /*
 Returns the first matched iterator, or ListEnd if case no match found.
 */
+dll_iter_t DoublyListFind(const doubly_list_t *list, void *param,
+                          int (*is_match)(const void *data, const void *param));
 
-int DoublyListFindAll(const doubly_list_t *list,
-                      void *param,
-                      int (*is_match)(const void *data, const void *param), doubly_list_t *output_list);
 /*
 On success returns 0;
 otherwise return 1.
 found data is appended to the end of output_list.
 In case of error mid-iteration, the data that was found until the error is appended.
 */
+int DoublyListFindAll(const doubly_list_t *list,
+                      void *param,
+                      int (*is_match)(const void *data, const void *param), doubly_list_t *output_list);
 
-dll_iter_t DoublyListForEach(doubly_list_t *list,
-                             void *param,
-                             int (*operation_func)(void *data, void *param));
 /*
 Iteration continues while operation_func returns 0.
 Returns ListEnd if iterated through the entire list,
 otherwise returns the iterator on which it was stopped
 */
+dll_iter_t DoublyListForEach(doubly_list_t *list,
+                             void *param,
+                             int (*operation_func)(void *data, void *param));
 
 #endif /* __DOUBLY_LINKED_LIST_H__ */
