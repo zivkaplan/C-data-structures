bbdecf1577096052cc34455e9315e6c8ae94c0f1
ziv.kaplan <zivkaplan@gmail.com>
2022-03-30 10:19:04 +0300

    added comments before declaration

--- circular_buffer/circular_buffer.h
+++ circular_buffer/circular_buffer.h
@@ -15,18 +15,20 @@
 
 typedef struct circular_buffer circ_buff_t;
 
-circ_buff_t *CircBuffCreate(size_t capacity_in_bytes);
 /*
 Returns NULL on failure.
 capacity_in_bytes == 0 is undefined.
 */
+circ_buff_t *CircBuffCreate(size_t capacity_in_bytes);
 
-void CircBuffDestroy(circ_buff_t *circ_buff);
 /* Calling Destroy(NULL) is valid */
+void CircBuffDestroy(circ_buff_t *circ_buff);
 
+/* returns the number of bytes read from the buffer. */
 size_t CircBuffRead(circ_buff_t *circ_buff, void *dest, size_t n_bytes);
+
+/* returns the number of bytes written to the buffer. */
 size_t CircBuffWrite(circ_buff_t *circ_buff, const void *src, size_t count);
-/* returns the number of bytes read\written from\to buffer. */
 
 size_t CircBuffCapacity(const circ_buff_t *circ_buff);
 size_t CircBuffFreeSpaceLeft(const circ_buff_t *circ_buff);
